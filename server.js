21aea65d36a34e6cdbe0666ef43cb54ada349aca
codewurld <sholaq@gmail.com>
2021-07-30 14:22:02 +0100

    using nodemon and lodash

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require("http");
 const fs = require('fs')
+const _ = require("lodash");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+
+    //lodash
+
+    const num = _.random(0, 20);
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log("chai")
+    })
+
+    greet();
+    greet();
+
+    // console.log(req.url, req.method)
 
     // set header content type
     res.setHeader("Content-Type", "text/html")
@@ -19,8 +33,9 @@ const server = http.createServer((req, res) => {
             path += "about.html";
             res.statusCode = 200;
             break;
-        case "/about-me":
+        case "/about-us":
             res.statusCode = 301;
+            //redirecting
             res.setHeader("Location", "/about")
             res.end()
             break;
