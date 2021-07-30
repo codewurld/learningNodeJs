448d17e79232e07df948a4ed1c32f10b1b7b8a30
codewurld <sholaq@gmail.com>
2021-07-30 11:49:22 +0100

    response objects and returning html

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const http = require("http");
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log("request made")
+    console.log(req.url, req.method)
+
+    // set header content type
+    res.setHeader("Content-Type", "text/html")
+
+    //write content -- but better way is to use fs.readFile from dedicated HTML
+
+    // res.write('<img src="https://images.unsplash.com/photo-1627521947524-f4d79a11437f?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=60">');
+    // res.write('<p>ma so ri re</p>')
+
+
+
+    // send an html file 
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+            // res.end above is important otherwise the brower hangs because request is never ending
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+
+    //this sends the response to the brower
+    // res.end();
 });
 
 server.listen(3000, "localhost", () => {
