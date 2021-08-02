e83542d34582bf76c4ff53ba7e9054ec4cd7e51d
codewurld <sholaq@gmail.com>
2021-08-02 14:08:29 +0100

    mongo basics -- creating models and schemas

--- viewengine.js
+++ viewengine.js
@@ -1,20 +1,49 @@
 const express = require('express');
-const morgan = require("morgan")
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require('./models/blog')
 
 //express app
 
 const app = express();
 
+// connect to mongoDB
+//useNew and useUnified is to stop deprecation errors logging
+// as mongoose.connect is async, we want to listen for our requests immediately after
+
+const dbURI = "mongodb+srv://doncodeo:londonparis0709@cluster0.kxnhi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
+
+// connect to viewengine
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000);
+// app.listen(3000);
 
 // middlewre & static files
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
 
+// mongoose and mongo sandbox routes
+// creating a new instance of blog to be added to the DB
+app.get("/add-blog", (req, res) => {
+    const blog = new Blog({
+        title: 'Learning Loads Part 2',
+        snippet: 'It takes a while to get going...',
+        body: "Finally, I understand what is meant by trust the process, see in life, a fulfilled and truly successful one, there are no shortcuts"
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 // working with middleware, must specify next() otherwise the browser hangs
 
